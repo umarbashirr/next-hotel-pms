b90bfbcbb2857e9783e0a9c25330d2c6916ae05b
umarbashirr <umarbashir93@outlook.com>
2024-06-23 14:58:03 +0530

    chore: Add logoutUser function for signing out

--- src/app/(auth)/actions.ts
+++ src/app/(auth)/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { signIn } from "@/auth";
+import { signIn, signOut } from "@/auth";
 import { AuthError } from "next-auth";
 
 export const loginUser = async (email: string, password: string) => {
@@ -12,6 +12,26 @@ export const loginUser = async (email: string, password: string) => {
       password,
       redirectTo: "/properties",
     });
+  } catch (error: any) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid email or password" };
+        default:
+          return { error: "An error occurred while signing in" };
+      }
+    }
+    throw error;
+  }
+};
+
+export const logoutUser = async () => {
+  try {
+    const logout = await signOut({ redirectTo: "/login" });
+
+    if (!logout) throw new Error("An error occurred while signing out");
+
+    return { success: true, message: "Successfully signed out" };
   } catch (error: any) {
     if (error instanceof AuthError) {
       switch (error.type) {
